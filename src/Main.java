30d9fe3cbf4cf96e94883e0c934b326455ec4385
omar123-jav <82762206+omar123-jav@users.noreply.github.com>
2021-12-31 21:29:41 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner;
+public class Main {
+public static String Interpret(String Instruction) {
+	String[] instruction=Instruction.split(" ");
+	String type="";
+	if(instruction[0].equals("L.D")) {
+	   type="load";
+	}else if(instruction[0].equals("S.D")) {
+		type="store";
+	}else if(instruction[0].equals("ADD.D")) {
+		type="add";
+	}else if(instruction[0].equals("SUB.D")) {
+		type="sub";
+	}else if(instruction[0].equals("MUL.D")) {
+		type="mult";
+	}else if(instruction[0].equals("DIV.D")) {
+		type="div";
+	}
+	if(type.equals("load")||type.equals("store")) {
+		String dstsrc=instruction[1].substring(0,instruction[1].length()-1);
+		int address=Integer.valueOf(instruction[2]);
+		return type+" "+dstsrc+" "+address;
+	}else {
+		String destination=instruction[1].substring(0,instruction[1].length()-1);
+		String operand1=instruction[2].substring(0,instruction[2].length()-1);
+		String operand2=instruction[3];
+		return type+" "+" "+destination+" "+operand1+" "+operand2;
+	}
+}
+public static void main(String[] args) throws FileNotFoundException {
+	 File instructions = new File("input3.txt");
+     Scanner myReader = new Scanner(instructions);
+     while (myReader.hasNextLine()) {
+       String instruction = myReader.nextLine();
+       System.out.println(Interpret(instruction));
+      }
+     myReader.close();
+}
+}
